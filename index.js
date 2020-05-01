dffe320ed8e1f60309d538be11253d1cb191ffe4
Kevin Kwok <antimatter15@gmail.com>
2020-05-01 01:41:51 -0700

    fixing behavior

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ async function main() {
 
     if (data.trim() !== target_branch) {
         let branch_name = await getBranchName()
-        data = await shell('git', [
-            'rev-list',
-            '--count',
-            '"^HEAD"',
-            '"^origin/' + branch_name + '"',
-        ])
+        data = await exec('git', ['rev-list', '--count', 'HEAD', '"^origin/' + branch_name + '"'])
         if (parseInt(data.trim()) != 0) throw 'ERROR: Unpushed changes in branch'
         await openPR(target_branch)
         return
